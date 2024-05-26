9f68b7c83882afd3ac92014ab1fb8195bdb9da32
Matt Doak <contact@mattdoak.ca>
2024-05-26 12:52:27 -0500

    more changes

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,37 @@
 import { defineAction, z } from "astro:actions";
+import { Analytics } from '@segment/analytics-node';
+
+const analytics = new Analytics({ writeKey: import.meta.env.SEGMENT_WRITE_KEY });
 
 export const server = {
   newsletter: defineAction({
     accept: "form",
     input: z.object({
       email: z.string().email(),
-      receivePromo: z.boolean(),
+
     }),
-    handler: async ({ email, receivePromo }) => {
-        
+    handler: async ({ email }) => {
+      analytics.track({
+        userId: '',
+        event: 'Newsletter Subscribed',
+        properties: {
+          email,
+        },
+      });
+
       // call a mailing service, or store to a database
       return { success: true };
     },
   }),
+  waitlist: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string().email(),
+    }),
+    handler: async ({ email }) => {
+
+      // call a mailing service, or store to a database
+      return { success: true };
+    },
+  })
 };
