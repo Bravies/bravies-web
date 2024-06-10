0f6bfd65f9b0b57b82cf6e6ed893346d724f7f20
Matt Doak <contact@mattdoak.ca>
2024-06-09 21:55:52 -0500

    Fixed posthog

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,6 @@
 interface Window {
   posthog: {
+    init: (token: string, options?: { api_host?: string, ui_host?: string, person_profiles?: string }) => void;
     capture: (event: string, data?: { [key: string]: any }) => void;
   }
 };
