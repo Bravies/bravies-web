709e46857f1f7d833429a5545d101b36cdc23568
Matt Doak <contact@mattdoak.ca>
2024-06-09 22:12:51 -0500

    Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,22 @@ import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import vercel from "@astrojs/vercel/serverless";
 import alpinejs from "@astrojs/alpinejs";
-
 import sitemap from "@astrojs/sitemap";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bravies.ca',
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), markdoc(), keystatic(), alpinejs(), sitemap()],
+  integrations: [sentry({
+    dsn: "https://835eeb41edf622fbc720d48da63cdcff@o4507405439401984.ingest.us.sentry.io/4507405442547712",
+    sourceMapsUploadOptions: {
+      project: "web",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  }),tailwind({
+    applyBaseStyles: false
+  }), react(), markdoc(), keystatic(), alpinejs(), sitemap(), sentry()],
   output: 'hybrid',
   adapter: vercel()
 });
---
+++ src/components/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import sentry from "@sentry/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sentry()]
+});
